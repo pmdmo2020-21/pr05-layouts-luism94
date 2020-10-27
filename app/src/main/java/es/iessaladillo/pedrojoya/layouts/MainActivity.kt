069a0425c3571cd1c4694f3b244cf5b891ada438
Luis Miguel <Merino Marn>
2020-10-27 13:40:52 +0100

    Version 1.0
    Subida del proyecto a GitHub

--- app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO: USAR UNO DE LOS LAYOUTS DEFINIDOS PARA PROBARLO.
-        setContentView(R.layout.linearlayout_activity)
+        //setContentView(R.layout.relative_layout_activity)
+        //setContentView(R.layout.linearlayout_activity)
+        setContentView(R.layout.constraint_layout_activity)
     }
 }
